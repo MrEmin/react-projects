b6835c23c5c6ee5296650afc4e615eebb0ca8e71
MrEmin <emuhammet.ozdemir@gmail.com>
2022-06-27 00:36:32 +0300

    Autoplay

--- 07-slider/src/App.js
+++ 07-slider/src/App.js
@@ -4,8 +4,26 @@ import { FaQuoteRight } from "react-icons/fa";
 import data from "./data";
 import people from "./data";
 function App() {
-  const [list, setList] = useState(data);
+  const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [index]);
+
   return (
     <section className="section">
       <div className="title">
